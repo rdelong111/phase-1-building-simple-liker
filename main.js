cf55e5c615cb388ef31dbb96c5f4bc4da8f5e956
RyanDeLong <rdelong111@gmail.com>
2021-12-30 14:40:56 -0600

    commit tree b52053d0504a35067f50b69d56187c868e9cf6d9

--- main.js
+++ main.js
@@ -1,11 +1,35 @@
 // Defining text characters for the empty and full hearts for you to use later.
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
+const EMPTY_HEART = '♡';
+const FULL_HEART = '♥';
 
 // Your JavaScript code goes here!
+const btns = document.querySelectorAll('.like-glyph');
+for (const btn of btns) { // goes through all the like buttons
 
-
-
+  btn.addEventListener('click', (e) => {
+    e.preventDefault();
+    mimicServerCall()
+      .then( () => {
+        // if the heart is empty
+        if (btn.textContent === EMPTY_HEART) {
+          btn.textContent = FULL_HEART;
+          btn.classList.add('activated-heart');
+          console.log('empty clicked');
+        }
+        //if the heart is full
+        else {
+          btn.textContent = EMPTY_HEART; 
+          btn.classList.remove('activated-heart');
+          console.log('filled clicked');
+        }
+      })
+      .catch( () => {
+        const errormsg = document.querySelector('#modal');
+        errormsg.removeAttribute('class');
+        setTimeout(() => errormsg.setAttribute('class', 'hidden'), 3000);
+      });
+  });
+};
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
